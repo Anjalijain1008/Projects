e6d625317b4726bef199fa46ed9027bfb5fabd6f
Anjalijain1008 <112240348+Anjalijain1008@users.noreply.github.com>
2023-05-01 13:05:05 +0530

    Add files via upload

---
+++ StudentCrud/src/main/java/com/demo/web/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.demo.web.repository;
+
+import com.demo.web.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
